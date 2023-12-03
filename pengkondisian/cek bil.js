fd286592aef942e80db26d43c676072921112c2f
Hazel368 <hazelmpr368@gmail.com>
2023-12-03 23:00:41 +0700

    js bengi

---
+++ pengkondisian/cek bil.js
@@ -0,0 +1,10 @@
+var angka = prompt ('Masukkan angka : ');
+
+if (angka % 2 === 0) {
+    alert('Bilangan itu Genap');
+} else if (angka % 2 === 1) {
+    alert('Bilangan itu Ganjil');
+
+} else {
+    alert('Gak valid ho');
+}
---
+++ pengkondisian/if else.js
@@ -0,0 +1,18 @@
+var jmlAngkot = 10;
+var noAngkot = 1;
+var angkotBeroperasi = 6;
+
+for (noAngkot = 1; noAngkot<= jmlAngkot; noAngkot++){
+    if (noAngkot<=angkotBeroperasi) {
+        console.log('Angkot No.' + noAngkot + ' Beroperasi dengan baik' );
+    } else {
+        if (noAngkot === 8) {
+            console.log('Angkot No.' + noAngkot + ' lagi lembur ygy');
+        } else {
+            console.log('Angkot No.' + noAngkot + ' sedang tidak beroperasi ygy');
+            
+        }
+        
+    }
+
+}
