9ad93e8144da1de01d677fa21a11e730e03725aa
Hazel368 <hazelmpr368@gmail.com>
2023-12-04 22:52:45 +0700

    update

---
+++ pengkondisian/switch.js
@@ -0,0 +1,19 @@
+var item = prompt('Lebokno pangananmu/ngombemu lur : \n (cth : Daging,Susu,Sego, Nuget,Arak,Babi )');
+
+switch (item) {
+    case 'Daging':
+        alert('Awamu milih protein');
+        break;
+    case 'Susu':
+        alert('Awamu milih sg akeh manfaat e');
+        break;
+    case 'Babi':
+        alert('Awamu milih panganan haram ');
+        break;
+    case 'Rokok':
+        alert('wes kenek kanker, duek tambah boncos');
+        break;
+    
+    default:
+        break;
+}
