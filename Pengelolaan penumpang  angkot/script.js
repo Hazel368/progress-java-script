37e0245a1ee114b41a0a1d8e1f4e3d577c719f32
Hazel368 <hazelmpr368@gmail.com>
2023-12-10 22:11:38 +0700

    gacor kang

---
+++ Pengelolaan penumpang  angkot/script.js
@@ -0,0 +1,46 @@
+var penumpang = [];
+var tambahPenumpang = function (namaPenumpang, penumpang) {
+    if (penumpang.length === 0 ) {
+        
+        penumpang.push(namaPenumpang);
+        
+        return penumpang;
+    } else {
+        for (var i = 0; i < penumpang.length; i++ ){
+            if (penumpang[i] === undefined) {
+               
+                penumpang[i] = namaPenumpang;
+
+                return penumpang;
+            }
+            else if (penumpang[i] == namaPenumpang) {
+                console.log('' + namaPenumpang + ' Jenenge wes onok lur ');
+            }
+            else if (i === penumpang.length - 1) {
+                penumpang.push(namaPenumpang);
+
+                return penumpang;
+            }
+
+        }
+    }
+}; // tambahkan kurung kurawal ini
+var hapusPenumpang = function (namaPenumpang, penumpang) {
+        if (penumpang.length === 0 ) {
+            console.log('angkot kosong ');
+
+            return penumpang;
+        } else {
+            for(var i = 0; i < penumpang.length; i++ ){
+                if (penumpang[i] === namaPenumpang){
+                    penumpang[i] = undefined;
+                    return penumpang;
+                }
+                else if (i === penumpang.length - 1){
+                    console.log(+ namaPenumpang + ' Gak onk jenenge lek ');
+                    return penumpang;
+                }
+            }
+            
+        }
+}
